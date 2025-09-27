e3e7876c9d2309e1633ed76f9fa38f95caa621aa
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-27 15:01:38 -0300

    feat: aprendendo o metodo splice

---
+++ atualizar-lista.js
@@ -0,0 +1,3 @@
+const listaEstudantes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+listaEstudantes.splice(1, 2, "Rodrigo"); // a partir do índice 1, remove 2 elementos e adiciona "Rodrigo"
+console.log(`Lista de estudantes atualizada: ${listaEstudantes}`);
