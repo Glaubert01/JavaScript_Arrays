03378a2c1e40059ef85ef057914180aacebc7f6f
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-27 15:17:16 -0300

    feat: trabalhando com array de arrays

---
+++ array-de-arrays.js
@@ -0,0 +1,7 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+const salaDeAula = [alunos, medias];
+console.log(salaDeAula);
+console.log(
+  `A aluna da posição 1 é ${salaDeAula[0][1]} e sua média é ${salaDeAula[1][1]}`
+);
