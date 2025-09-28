eeb1584eb41a218765078de0eac196eb232206c5
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-28 15:06:13 -0300

    update: trabalhando com sets

---
+++ set.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "Joao", "Joao", "Joao"];
+
+// const nomesAtualizados = new Set(nomes);
+
+const listaNomesAtualizados = [...new Set(nomes)];
+
+console.log(listaNomesAtualizados);
