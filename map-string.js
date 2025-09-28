7d33fde2a96dd878609a957b4b980460347e6794
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-27 21:44:28 -0300

    update: utilizando map com strings

---
+++ map-string.js
@@ -0,0 +1,6 @@
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+const nomesAtualizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesAtualizados);
+console.log(nomes);
